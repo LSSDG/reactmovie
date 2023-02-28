e828d9c1ebae4684f21640b6af24368f7d7ec7d9
lssdg <lssdannygiftson@gmail.com>
2023-02-28 11:39:04 +0530

    staticproducts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+ import {Button,Container,Row,Col,Navbar,Nav,Image} from 'react-bootstrap';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Collapse>
+        <Nav>
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">Store</Nav.Link>
+          <Nav.Link href="#">About</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+      <Button>CART</Button>
+      <h3>0</h3>
+    </Navbar>
+    <Navbar align="center"  bg='light' variant='success'><Navbar.Brand ><h1>REACT MOVIE</h1></Navbar.Brand></Navbar>
+      <Container>
+        <Row>
+          <h2>MUSIC</h2>
+          <Col>
+          <Image src="https://prasadyash2411.github.io/ecom-website/img/Album%201.png"></Image>
+          <h3>$19.99</h3>
+          <Button>ADD TO CART</Button></Col>
+          <Col>
+          <Image src="https://prasadyash2411.github.io/ecom-website/img/Album%202.png"></Image>
+          <h3>$14.99</h3>
+          <Button>ADD TO CART</Button>  
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <Image src="https://prasadyash2411.github.io/ecom-website/img/Album%203.png"></Image>
+          <h3>$29.99</h3>
+          <Button>ADD TO CART</Button></Col>
+          <Col>
+          <Image src="https://prasadyash2411.github.io/ecom-website/img/Album%204.png"></Image>  
+          <h3>$12.99</h3>
+          <Button>ADD TO CART</Button>
+          </Col>
+        </Row>
+
+      </Container>  
+      <h1></h1>
+      <Container><Button>SEE THE CART</Button></Container>
+      <footer>REACT MOVIE</footer>
+       
     </div>
   );
 }
